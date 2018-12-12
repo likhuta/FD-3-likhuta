96c79b34be6081474110c6d08e0bab500aa682b0
annalikhuta <annalikhuta@gmail.com>
2018-12-13 01:33:52 +0300

    добавила картинки к каждой позиции товара

--- ishop/ProductString.js
+++ ishop/ProductString.js
@@ -7,12 +7,15 @@ var ProductString= React.createClass({
     React.DOM.span({className:'MyComponentText'},  'Наименование: '),
  
     React.DOM.span({className:'MyComponentText'},  this.props.name),
-    React.DOM.span({className:'MyComponentText'},  'Стоимость: '),
+    React.DOM.span({className:'MyComponentText'},  '   Стоимость: '),
 
     React.DOM.span({className:'MyComponentText'},  this.props.cost),
-    React.DOM.span({className:'MyComponentText'},  'Количество в наличии: '),
+    React.DOM.span({className:'MyComponentText'},  '   Количество в наличии: '),
 
     React.DOM.span({className:'MyComponentText'},  this.props.count),
+
+   React.DOM.br(),
+    React.DOM.img({src:'src/'+this.props.name+'.jpg'}),
 
      )
 
