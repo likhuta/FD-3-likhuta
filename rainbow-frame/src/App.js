63210c9cf0c90ea3c61c1ea9e26ce4f38f392e54
annalikhuta <annalikhuta@gmail.com>
2019-01-14 01:07:27 +0300

    BR

--- rainbow-frame/src/App.js
+++ rainbow-frame/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import RainbowFrame from  './RainbowFrame'
+import BR from  './BR'
+
 
 class App extends Component {
   render() {
    let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-
+   let text="первый<br>второй<br/>третий<br />последний";
    
   return (
+    <span> 
     <RainbowFrame colors={colors}>
     
       Hello!
    
     </RainbowFrame>
+    {
+      // второе задание 
+    }
+    <BR text={text} />
+    </span>
   );
   }
 }
---
+++ rainbow-frame/src/BR.jsx
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import './BR.css';
+
+class BR extends Component {
+  render(){
+  let textStr=this.props.text;
+  let textArr=textStr.split(/<.*?>/g);
+    let test=[]
+    for(let i=0; i<=textArr.length-1; i++){
+    if(i) {
+      test.push(<br/>)
+    }
+    test.push(textArr[i] );
+    }
+    return(
+      <div>
+      {test}
+      </div>
+    )
+
+  }
+
+}
+
+export default BR
