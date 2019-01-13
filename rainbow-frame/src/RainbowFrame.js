a57c0c70b29b7aa19fb5732ab385dd36af8f4701
annalikhuta <annalikhuta@gmail.com>
2019-01-13 22:58:00 +0300

    correct task

--- rainbow-frame/src/RainbowFrame.js
+++ rainbow-frame/src/RainbowFrame.js
@@ -12,30 +12,37 @@ class RainbowFrame extends Component {
   }
 
   render(){
-    
+    /*
     let colors =this.props.colors;
-    var answerCode=[];
-    colors.map((item,i)=>{
-      let abc=
-      <div style={{border:"solid 2px "+colors[i],padding:"10px"}}>
-       {answerCode} {
-        (i)
-        ?null
-        :this.props.children
-        }
-      </div>
-    
-  return  answerCode=abc;
-    })
+    var answerCode=this.props.children;
 
+    for(let i=0; i<=colors.length-1; i++){
+      answerCode=<div style={{border:"solid 3px "+colors[i],padding:"10px"}} >
+        {answerCode }
+      </div>
+    }
     return (
         <div> 
         { answerCode}
         </div>
-
       )
+  */
+ let colors =this.props.colors;
 
-  }
+  return(
+    
+  <div style={{border:"solid 3px "+colors[0],padding:"10px"}}  >
+      {
+        (colors.length!==0)
+        ?  <RainbowFrame colors={colors.slice().splice(1,colors.length-1)} >
+            {this.props.children}
+           </RainbowFrame>
+        :   this.props.children
+      }
+    </div>
+
+  )
+}
 }
 
 export default RainbowFrame;
