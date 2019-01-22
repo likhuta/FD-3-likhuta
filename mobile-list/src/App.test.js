cadcb19ed59aa1776a7e1fefbf39cc15ff3e7750
annalikhuta <annalikhuta@gmail.com>
2019-01-23 00:33:51 +0300

    test

--- mobile-list/src/App.test.js
+++ mobile-list/src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import ReactDOM from 'react-dom';
+import App from '../src/App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+   // создаём тестовую версию компонента
+   const component = renderer.create(
+    <App />
+  );
+
+ 
+  // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+    // найдём в вёрстке компонента саму кнопку
+    const buttonElem = component.root.find( el => el.props.id==1 /*&& el.props.aaa == 'bbb'*/ ); 
+    // и "нажмём" на неё
+    buttonElem.props.onClick();
+  
+    // получаем уже изменённый снэпшот
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+  
+
+
+ 
 });
+
